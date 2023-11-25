c41231033c311cbead6c0bf2033164003c91fa42
Alejandro <alejoramvids@gmail.com>
2023-11-24 18:56:18 -0600

    Ejercicios resueltos

--- PAT_HojaDeTrabajo03/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio02.cpp
@@ -1,6 +1,29 @@
 #include "Ejercicio02.h"
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
 
 vector<vector<string>>* Ejercicio02::groupAnagrams(vector<string>& strings)
 {
-    return nullptr;
+    unordered_map<string, vector<string>> anagramMap;
+
+    for (const string& word : strings)
+    {
+        string sortedWord = word;
+        sort(sortedWord.begin(), sortedWord.end());
+
+        anagramMap[sortedWord].push_back(word);
+    }
+
+    vector<vector<string>>* result = new vector<vector<string>>();
+    for (const auto& entry : anagramMap)
+    {
+        result->push_back(entry.second);
+    }
+
+    return result;
 }
+
